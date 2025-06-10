e5de4d7d9e54e5574b005b65002c70a201734e3a
KillLaAqua <KillLaAqua@gmail.com>
2025-06-10 13:41:56 -0500

    Solar Finalized

--- kubejs/server_scripts/gregification/solar_energy.js
+++ kubejs/server_scripts/gregification/solar_energy.js
@@ -2,68 +2,71 @@ ServerEvents.recipes(event => {
     const id = global.id;
 
     event.recipes.gtceu.circuit_assembler(id('basic_energy_core'))
-        .itemInputs('gtceu:silicon_wafer', '4x #gtceu:capacitors', '2x #gtceu:diodes', 'gtceu:tin_single_cable', '4x minecraft:glass_pane')
-        .inputFluids('gtceu:soldering_alloy 12')
+        .itemInputs('3x gtceu:iron_foil', '2x #gtceu:capacitors', '2x #gtceu:diodes')
+        .inputFluids('gtceu:glass 288')
         .itemOutputs('kubejs:basic_energy_core')
         .duration(200)
-        .EUt(8);
+        .EUt(30);
 
-    event.recipes.gtceu.polarizer(id('basic_to_regular_energy_core'))
-        .itemInputs('kubejs:basic_energy_core')
-        .itemOutputs('kubejs:regular_energy_core')
-        .duration(1000)
-        .EUt(25);
+    const CoreUpgrade = (In,Out,Tier) => {
+        event.recipes.gtceu.polarizer(id(`${In}_to_${Out}_energy_core`))
+            .itemInputs(`kubejs:${In}_energy_core`)
+            .itemOutputs(`kubejs:${Out}_energy_core`)
+            .duration(400 * (2 ** Tier))
+            .EUt(30 * (4 ** Tier));
+    }
+    CoreUpgrade('basic','regular',1);
+    CoreUpgrade('regular','intermediate',2);
+    CoreUpgrade('intermediate','advanced',3);
+    CoreUpgrade('advanced','elite',4);
+    CoreUpgrade('elite','ultimate',5);
 
-    event.recipes.gtceu.polarizer(id('regular_to_intermediate_energy_core'))
-        .itemInputs('kubejs:regular_energy_core')
-        .itemOutputs('kubejs:intermediate_energy_core')
-        .duration(2000)
-        .EUt(125);
+    event.recipes.gtceu.assembler(id('mirror'))
+        .itemInputs('6x gtceu:steel_foil', '#gtceu:circuits/lv', 'gtceu:glass_plate')
+        .inputFluids('gtceu:tin 576')
+        .itemOutputs('solarflux:mirror')
+        .duration(400)
+        .EUt(30);
 
-    event.recipes.gtceu.polarizer(id('intermediate_to_advanced_energy_core'))
-        .itemInputs('kubejs:intermediate_energy_core')
-        .itemOutputs('kubejs:advanced_energy_core')
-        .duration(5000)
-        .EUt(500);
-
-    event.recipes.gtceu.polarizer(id('advanced_to_elite_energy_core'))
-        .itemInputs('kubejs:advanced_energy_core')
-        .itemOutputs('kubejs:elite_energy_core')
-        .duration(12500)
-        .EUt(2000);
-
-    event.recipes.gtceu.polarizer(id('elite_to_ultimate_energy_core'))
-        .itemInputs('kubejs:elite_energy_core')
-        .itemOutputs('kubejs:ultimate_energy_core')
-        .duration(31250)
-        .EUt(8000);
-
-    const SolarPanel = (type, material, base, tier, chip, wire, cable, core) => {
+    const SolarPanel = (type, material, base, tier, chip, wire, core) => {
         let cell = (base == 'mirror') ? 'mirror' : `photovoltaic_cell_${type - 1}` ;
         event.recipes.gtceu.assembler(id(`solar_generator_${type}`))
-            .itemInputs(`gtceu:${material}_frame`, `solarflux:${cell}`, `gtceu:${material}_plate`, `#gtceu:circuits/${tier}`, `2x gtceu:${cable}_single_cable`)
+            .itemInputs(`gtceu:${material}_frame`, `solarflux:${cell}`, `#gtceu:circuits/${tier}`, `2x gtceu:${wire}_single_cable`, `2x gtceu:${material}_plate`)
             .inputFluids(`gtceu:soldering_alloy ${144 * type}`)
             .itemOutputs(`solarflux:sp_${type}`)
             .duration(300)
             .EUt(30 * (4 ** type));
 
+        event.recipes.gtceu.electromagnetic_separator(id(`solar_generator_${type}_decomp`))
+            .itemInputs(`solarflux:sp_${type}`)
+            .itemOutputs(`gtceu:${material}_frame`, `solarflux:${cell}`, `kubejs:${tier}_universal_circuit`)
+            .duration(200)
+            .EUt(30 * (4 ** (type - 1)));
+
         if (type !== 1) {
         let prior = (type == 2) ? 'mirror' : `photovoltaic_cell_${type - 2}` ;
         event.recipes.gtceu.circuit_assembler(id(`photovoltaic_cell_${type - 1}`))
-            .itemInputs(`solarflux:${prior}`, `kubejs:${chip}_chip`, `2x #gtceu:circuits/${tier}`, `kubejs:${core}_core`, `6x gtceu:fine_${wire}_wire`)
+            .itemInputs(`solarflux:${prior}`, `kubejs:${chip}_chip`, `2x #gtceu:circuits/${tier}`, `kubejs:${core}_energy_core`, `6x gtceu:fine_${wire}_wire`)
             .inputFluids(`gtceu:soldering_alloy ${72 * type}`)
-            .itemOutputs(`solarflux:photovoltaic_cell_${type}`)
+            .itemOutputs(`solarflux:photovoltaic_cell_${type - 1}`)
             .duration(400)
             .EUt(30 * (4 ** (type - 1)));
         }
         }
     
-    SolarPanel('1','aluminium','mirror','mv','','copper','copper','basic')
-    SolarPanel('2','stainless_steel','','hv','silicon','copper','copper','basic')
-    SolarPanel('3','titanium','','ev','silicon','copper','copper','basic')
-    SolarPanel('4','tungsten_steel','','iv','phosphorus','copper','copper','basic')
-    SolarPanel('5','rhodium_plated_palladium','','luv','phosphorus','copper','copper','basic')
-    SolarPanel('6','naquadah_alloy','','zpm','naquadah','copper','copper','basic')
-    SolarPanel('7','darmstadtium','','uv','naquadah','copper','copper','basic')
+    SolarPanel('1','aluminium','mirror','mv','','copper','')
+    SolarPanel('2','stainless_steel','','hv','silicon','gold','basic')
+    SolarPanel('3','titanium','','ev','silicon','aluminium','regular')
+    SolarPanel('4','tungsten_steel','','iv','phosphorus','platinum','intermediate')
+    SolarPanel('5','rhodium_plated_palladium','','luv','phosphorus','niobium_titanium','advanced')
+    SolarPanel('6','naquadah_alloy','','zpm','naquadah','vanadium_gallium','elite')
+    SolarPanel('7','darmstadtium','','uv','naquadah','yttrium_barium_cuprate','ultimate')
+
+    // T8 Solar Decomp (so it has some use in Eta [aka can be decomped])
+        event.recipes.gtceu.electromagnetic_separator(id(`solar_generator_8_decomp`))
+            .itemInputs(`solarflux:sp_8`)
+            .itemOutputs(`solarflux:photovoltaic_cell_6`, `kubejs:uv_universal_circuit`, `2x gtceu:darmstadtium_plate`)
+            .duration(200)
+            .EUt(30 * (4 ** 7));
 
 });
