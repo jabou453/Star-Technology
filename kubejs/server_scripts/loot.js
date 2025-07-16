5fa2102138bcef915496557564d4356d8863886c
KillLaAqua <KillLaAqua@gmail.com>
2025-07-16 00:26:56 -0500

    Fixes

--- kubejs/server_scripts/loot.js
+++ kubejs/server_scripts/loot.js
@@ -1,19 +1,39 @@
 LootJS.modifiers((event) => {
     
-    const abydosTemple = [
+    const abydosTempleTomb = [
         {loot: 'minecraft:gold_block', chance: .1},
         {loot: 'minecraft:enchanted_golden_apple', chance: .05},
         {loot: 'start_core:bacteria_dormant', chance: .15},
-        {loot: 'kubejs:runic_tablet_1', chance: .05},
-        {loot: 'kubejs:runic_tablet_2', chance: .05},
-        {loot: 'kubejs:runic_tablet_3', chance: .05},
-        {loot: 'kubejs:runic_tablet_4', chance: .05},
-        {loot: 'kubejs:runic_tablet_5', chance: .05},
-        {loot: 'kubejs:runic_tablet_6', chance: .05}
+        {loot: 'kubejs:runic_tablet_1', chance: .08},
+        {loot: 'kubejs:runic_tablet_2', chance: .08},
+        {loot: 'kubejs:runic_tablet_3', chance: .08},
+        {loot: 'kubejs:runic_tablet_4', chance: .08},
+        {loot: 'kubejs:runic_tablet_5', chance: .08},
+        {loot: 'kubejs:runic_tablet_6', chance: .08}
         ];
-    abydosTemple.forEach(templeLoot => {
+    abydosTempleTomb.forEach(templeLoot => {
         event
         .addLootTableModifier('sgjourney:chests/desert_pyramid_loot_room')
+        .randomChance(templeLoot.chance)
+        .addLoot(templeLoot.loot);
+    });
+
+    const abydosTempleSpawnerRoom = [
+        {loot: 'sgjourney:naquadah_helmet', chance: .1},
+        {loot: 'sgjourney:naquadah_chestplate', chance: .1},
+        {loot: 'sgjourney:naquadah_leggings', chance: .1},
+        {loot: 'sgjourney:naquadah_boots', chance: .1},
+        {loot: 'sgjourney:naquadah_sword', chance: .1},
+        {loot: 'sgjourney:naquadah_pickaxe', chance: .1},
+        {loot: 'sgjourney:naquadah_axe', chance: .1},
+        {loot: 'sgjourney:naquadah_shovel', chance: .1},
+        {loot: 'sgjourney:naquadah_alloy', chance: .1},
+        {loot: '2x sgjourney:naquadah_alloy', chance: .05},
+        {loot: '3x sgjourney:naquadah_alloy', chance: .02},
+    ];
+    abydosTempleSpawnerRoom.forEach(templeLoot => {
+        event
+        .addLootTableModifier('sgjourney:chests/desert_pyramid_challenge_room')
         .randomChance(templeLoot.chance)
         .addLoot(templeLoot.loot);
     });
