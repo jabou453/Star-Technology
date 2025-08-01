ec22c71a4226f27bab3cadecf59dcff4397e3335
KillLaAqua <KillLaAqua@gmail.com>
2025-08-01 16:42:38 -0500

    Runic Wetware Assets + Misc

--- kubejs/startup_scripts/items/wetware_variant_circuits.js
+++ kubejs/startup_scripts/items/wetware_variant_circuits.js
@@ -1,6 +1,23 @@
 
 StartupEvents.registry('item', event => {
 
+    // Runic-Wetwares
+    event.create('runic_wetware_processor_assembly')
+        .displayName('Runic Wetware Processor Assembly')
+        .tooltip('§eAn Ancient Civilization\'s UV Circuit')
+        .texture('kubejs:item/circuits/runic_wetware_processor_assembly');
+
+    event.create('runic_wetware_processor_computer')
+        .displayName('Runic Wetware Processor Supercomputer')
+        .tooltip('§eAn Ancient Civilization\'s UHV Circuit')
+        .texture('kubejs:item/circuits/runic_wetware_processor_computer');
+
+    event.create('runic_wetware_processor_mainframe')
+        .displayName('Runic Wetware Processor Mainframe')
+        .tooltip('§eAn Ancient Civilization\'s UEV Circuit')
+        .texture('kubejs:item/circuits/runic_wetware_processor_mainframe');
+
+    // Draconic Parts
     event.create('draconic_wetware_circuit_board')
         .displayName('Draconic Wetware Circuit Board')
         .texture('kubejs:item/circuits/draconic_wetware_circuit_board');
@@ -14,6 +31,7 @@ StartupEvents.registry('item', event => {
         .tooltip(`§d§oNeuro-sama!§r§7's CPU`)
         .texture('kubejs:item/circuits/draconic_neuro_processing_unit');
 
+    // Draconic-Wetwares
     event.create('draconic_wetware_microchip_processor')
         .displayName('Draconic Wetware Microchip Processor')
         .tooltip('§5Dr. Conic approved ZPM Circuit')
@@ -39,14 +57,31 @@ StartupEvents.registry('item', event => {
         .tooltip('§5Dr. Conic approved UIV Circuit')
         .texture('kubejs:item/circuits/draconic_wetware_processor_mainframe');
 
-    //UEV Wetware
+    // Awakened-Draconic-Wetwares
+    event.create('awakened_draconic_wetware_processor_assembly')
+        .displayName('Awakened Draconic Wetware Processor Assembly')
+        .tooltip('§5Dr. Conic unleased UEV Circuit')
+        .texture('kubejs:item/circuits/awakened_draconic_wetware_processor_assembly');
+
+    event.create('awakened_draconic_wetware_processor_computer')
+        .displayName('Awakened Draconic Wetware Processor Supercomputer')
+        .tooltip('§5Dr. Conic unleased UIV Circuit')
+        .texture('kubejs:item/circuits/awakened_draconic_wetware_processor_computer');
+
+    event.create('awakened_draconic_wetware_processor_mainframe')
+        .displayName('Awakened Draconic Wetware Processor Mainframe')
+        .tooltip('§5Dr. Conic unleased UXV Circuit')
+        .texture('kubejs:item/circuits/awakened_draconic_wetware_processor_mainframe');
+
+    // UEV Wetware //Recipe being removed, this is being depricated over Eta-2
     event.create('wetware_based_runic_neuroloom')
         .displayName('Wetware-based Runic Neuroloom')
         .tooltip('§7Wetware Pushed to the §6§oEXTREME')
         .tooltip('§4UEV-Tier Circuit')
+        .tooltip('§7Being Depricated over Eta-2')
         .texture('kubejs:item/circuits/wetware_based_runic_neuroloom');
 
-    //Draco-QMDs
+    // Draco-QMDs
     event.create('draconic_qmd_transistor')
         .displayName('Draconic QMD Transistor')
         .tooltip('§7Draco-Quantum Electronic Circuit')
