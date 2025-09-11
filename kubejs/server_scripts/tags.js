f55dd5e787adc96aa0a99521ab43a4415cd077e6
UltraPuPower1 <ultrapupower1@gmail.com>
2025-09-11 17:51:59 +0200

    Bugfixes - Realgar Geode tag - Potion crafts consume buckets - Bulk Blast Chiller quest description updated - Rubber quests to one completed - Startup log spam due to .material() - No mega co-processor recipe - Crystal Processor Mainframe now requires 16 CWU instead of 32 again - Framed drawers require empty tags - Fix missing optional on buckets

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -27,7 +27,7 @@ ServerEvents.tags('item', event => {
     event.add('exnihilosequentia:mesh', 'exnihilosequentia:netherite_mesh');
 
     ['diamond', 'emerald', 'ruby', 'sapphire', 'quartzite', 'topaz', 'spessartine', 'certus_quartz', 'green_sapphire'
-        ,'blue_topaz','apatite', 'monazite'].forEach(type => {
+        ,'blue_topaz','apatite', 'monazite', 'realgar'].forEach(type => {
         event.add('gtceu:geode', `kubejs:${type}_geode`);
         // event.add('gtceu:cracked_geode', `kubejs:cracked_${type}_geode`);
     });
@@ -42,9 +42,9 @@ ServerEvents.tags('item', event => {
     event.add('curios:back', 'kubejs:end_talisman');
 
     ['oak', 'spruce', 'birch', 'jungle', 'acacia', 'dark_oak', 'crimson', 'warped', 'mangrove', 'cherry'].forEach(type => {
-	event.add('functionalstorage:drawers_size_1',`functionalstorage:${type}_1`)
-	event.add('functionalstorage:drawers_size_2',`functionalstorage:${type}_2`)
-	event.add('functionalstorage:drawers_size_4',`functionalstorage:${type}_4`)
+	    event.add('functionalstorage:drawers_size_1',`functionalstorage:${type}_1`);
+	    event.add('functionalstorage:drawers_size_2',`functionalstorage:${type}_2`);
+	    event.add('functionalstorage:drawers_size_4',`functionalstorage:${type}_4`);
     });//for framed drawers
   
     //Vintage Tag Removals
